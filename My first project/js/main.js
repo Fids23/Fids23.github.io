a8d9d4401c1cf162a4f044df3436ef5cbb1a2476
Fids23 <90930419+Fids23@users.noreply.github.com>
2021-10-30 22:45:55 +0300

    First js
    Promt, alert

---
+++ My first project/js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+let name = prompt("What is your name?");
+let site = confirm("Do you want a site?")
+if (site == true) {
+	alert("Select item by number");
+}
+else {
+	alert("=(")
+}
+/*let site = prompt("What type of site do you want?/n 1)visit");
+console.log(site);
+*/
